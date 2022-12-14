287470f5ea5e22969e0b30bef521cea87ede1e62
Adityacraft1 <90310273+Adityacraft1@users.noreply.github.com>
2022-12-14 16:39:33 +0530

    Update 3 idiots project v2.py
    cleaned up the code

--- 3 idiots project v2.py
+++ 3 idiots project v2.py
@@ -1,27 +1,41 @@
-avgMarks = 0
 while True:
+    try:
+        choice = input("would you like to continue?(Y/N): ")
+        if choice.lower() == "y":
+            print("continuing")
+        elif choice.lower() == "n":
+            print("ending code")
+            print("updating files")
+            break
+        else:
+            print("invalid input")
+            continue
+
+    except:
+        print("invalid input")
     try:
         names = [str(x) for x in input("names: ").split(", ")]
     except:
         print("invalid input")
 
     for i in names:
+        i = i.capitalize()
         file = open(f"{i}.txt", "w")
         mathsMarks = float(input(f"please input maths marks for {i}: "))
         csMarks = float(input(f"please input computer science marks for {i}: "))
         englishMarks = float(input(f"please input english marks for {i}: "))
 
         if mathsMarks in range(0, 101) and csMarks in range(0, 101) and englishMarks in range(0, 101):
+            global avgMarks
             avgMarks = ((mathsMarks + csMarks + englishMarks) / 3)
-
-            if int(avgMarks) in range(90, 100):
-                grade = "A"
-            elif int(avgMarks) in range(80, 90):
-                grade = "B"
-            elif int(avgMarks) in range(70, 80):
-                grade = "C"
-            elif int(avgMarks) in range(60, 70):
-                grade = "D"
+            marksDict = {
+                range(90, 101): "A",
+                range(80, 90): "B",
+                range(70, 80): "C",
+                range(60, 70): "D",
+            }
+            if avgMarks in marksDict:
+                grade = marksDict[avgMarks]
             else:
                 grade = "Fail"
         else:
@@ -36,17 +50,3 @@ while True:
         file.write(f"average marks: {round(avgMarks, 2)}\n")
         file.write(f"grade: {grade}")
         file.close()
-
-    try:
-        choice = input("would you like to continue?(Y/N): ")
-        if choice.lower() == "y":
-            print("continuing")
-        elif choice.lower() == "n":
-            print("ending code")
-            break
-        else:
-            print("invalid input")
-            break
-
-    except:
-        print("invalid input")
