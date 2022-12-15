7dce0bc35fefe0f9a08468889c1da7676fd83e72
Adityacraft1 <90310273+Adityacraft1@users.noreply.github.com>
2022-12-15 22:42:56 +0530

    Update 3 idiots project v2.py
    im too bummed out to find the error in the previous code so i reverted it back to the original

--- 3 idiots project v2.py
+++ 3 idiots project v2.py
@@ -1,73 +1,52 @@
-# To ensure that the code keeps running.
 while True:
-    # Try ensures that the user cannot cause an error.
     try:
-        # choice is the input required.
-        choice = input("Would you like to continue? (Y/N) : ")
-        # if choice is yes, the code will continue.
-        if choice.lower() == "y" or 'yes':
-            print("Okay! Continuing... \n")
-        # if the choice is no, the code will break and the files will be updated.
-        if choice.lower() == "n" or 'no':
-            print("Okay! Updating Files... \n")
-            print("All done! Closing... \n")
+        choice = input("would you like to continue?(Y/N): ")
+        if choice.lower() == "y":
+            print("continuing")
+        elif choice.lower() == "n":
+            print("ending code")
+            print("updating files")
             break
-        # if the choice is neither yes nor no, an error message will be displayed.
         else:
-            print("The answer must be yes or no")
+            print("invalid input")
             continue
-    # Except condition for the Try condition
+
     except:
-        print("Invalid Input.")
-    # Try is used to get all the names of the students.
+        print("invalid input")
     try:
-        names = [str(x) for x in input("Enter the names of the students here, seperating with a comma and space. (Example: Aditya, Rathiin, Ihit, Vivyan): ").split(", ")]
-    # Except for the Try condition is used to make sure that the user has inputted the names correctly.
+        names = [str(x) for x in input("names: ").split(", ")]
     except:
-        print("Invalid Input. (Did you add a comma and space between the names?) ")
-    # For loop used for inputting the percentages of the students, calculating their average marks, getting their grades, and inputting it all into a file.
+        print("invalid input")
+
     for i in names:
-        # i = i.capitalize() is not required, it is just used for cosmetic purposes.
         i = i.capitalize()
-        # Opens a file with the name of the student.
         file = open(f"{i}.txt", "w")
-        # Checks if the input is a number and prevents an error.
-        try:
-            # Asks for the subject percentages of each student.
-            mathsMarks = float(input(f"Please enter the Maths percentage of {i}: "))
-            csMarks = float(input(f"Please enter the Computer Science percentage of {i}: "))
-            englishMarks = float(input(f"Please enter the English percentage of {i}: "))
-            # Checks if the input is in the acceptable range.
-            if mathsMarks in range(0, 101) and csMarks in range(0, 101) and englishMarks in range(0, 101):
-                # Declares avgMarks as a global variable to avoid undefined errors.
-                global avgMarks
-                # Calculates the average marks of each student.
-                avgMarks = ((mathsMarks + csMarks + englishMarks) / 3)
-                # Dictionary used to check for grade thresholds to prevent messy (if) statements.
-                marksDict = {
-                    range(90, 101): "A",
-                    range(80, 90): "B",
-                    range(70, 80): "C",
-                    range(60, 70): "D",
-                }
-                # Checks if the grade thresholds apply to the average.
-                if avgMarks in marksDict:
-                    grade = marksDict[avgMarks]
-                # If the average is not in the grade thresholds, the student has a failing grade.
-                else:
-                    grade = "Fail"
-            # Error for incorrect input (if the number is not in the range.)
+        mathsMarks = float(input(f"please input maths marks for {i}: "))
+        csMarks = float(input(f"please input computer science marks for {i}: "))
+        englishMarks = float(input(f"please input english marks for {i}: "))
+
+        if mathsMarks in range(0, 101) and csMarks in range(0, 101) and englishMarks in range(0, 101):
+            global avgMarks
+            avgMarks = ((mathsMarks + csMarks + englishMarks) / 3)
+            marksDict = {
+                range(90, 101): "A",
+                range(80, 90): "B",
+                range(70, 80): "C",
+                range(60, 70): "D",
+            }
+            if avgMarks in marksDict:
+                grade = marksDict[avgMarks]
             else:
-                print(" ")
-                print("The number has to be above 0 and below 100")
-                    # File writing.
-            file.write("DELHI PUBLIC SCHOOL\n")
-            file.write("-------------------\n")
-            file.write(f"The maths percentage of {i} is: {mathsMarks}\n")
-            file.write(f"The computer science percentage of {i} is: {csMarks}\n")
-            file.write(f"The english percentage {i} is: {englishMarks}\n")
-            file.write(f"The average of {i} is: {round(avgMarks, 2)}. This is a/an {grade}. \n")
-            file.close()
-        # Except for the try statement. Tells the user that the input is invalid.
-        except:
-            print("Invalid Input. The percentages must be a number.")
+                grade = "Fail"
+        else:
+            print(" ")
+            print("The number has to be above 0 and below 100")
+
+        file.write("DELHI PUBLIC SCHOOL\n")
+        file.write("-------------------\n")
+        file.write(f"maths marks for {i}: {mathsMarks}\n")
+        file.write(f"CS marks for {i}: {csMarks}\n")
+        file.write(f"english marks for {i}: {englishMarks}\n")
+        file.write(f"average marks: {round(avgMarks, 2)}\n")
+        file.write(f"grade: {grade}")
+        file.close()
